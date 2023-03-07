03bd4bdbd5b8191d86a66f9d590ed64f4e092914
amanmdesai <amanmukeshdesai@gmail.com>
2023-03-07 18:26:53 +0530

    add samples

---
+++ samples/backgroundpythia.cmd
@@ -0,0 +1,18 @@
+! File: lhe2pythia.cmnd
+Random:setSeed = on
+Main:timesAllowErrors = 5         ! how many aborts before run stops
+Main:numberOfEvents = 100000         ! number of events to generate
+
+
+! 2) Settings related to output in init(), next() and stat().
+Next:numberCount = 100             ! print message every n events
+!Beams:idA = 11                     ! first beam, e+ = 11
+!Beams:idB = -11                    ! second beam, e- = -11
+
+Beams:frameType = 4                ! read info from a LHEF
+! Change the LHE file here
+Beams:LHEF = background.lhe
+
+! 3) Settings for the event generation process in the Pythia8 library.
+PartonLevel:ISR = on               ! initial-state radiation
+PartonLevel:FSR = on               ! final-state radiation
